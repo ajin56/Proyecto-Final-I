26817c1d9e57f40be99b6aa9a0f2b280764f1961
ajin56 <35588376+ajin56@users.noreply.github.com>
2018-03-26 20:45:27 -0600

    Primer Commit
    - Diseño menú principal
    - Diseño de pestaña segundaría (Ingreso vehiculos)
    - Creación de clase Vehículos

---
+++ WindowsFormsApp1/WindowsFormsApp1/Class1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public abstract class Vehiculo
+    {
+        public String placa;
+        public String nombre;
+        public String nit;
+
+        public Vehiculo()
+        {
+
+        }
+        public Vehiculo(String Placa, String Nombre, String Nit)
+        {
+            this.placa = Placa;
+            this.nombre = Nombre;
+            this.nit = Nit;
+        }
+        public string Placa
+        {
+            get
+            {
+                return placa;
+            }
+            set
+            {
+                placa = value;
+            }
+        }
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                nombre = value;
+            }
+        }
+        public string Nit
+        {
+            get
+            {
+                return nit;
+            }
+            set
+            {
+                nit = value;
+            }
+        }
+    }
+    public class Moto : Vehiculo
+    {
+        public Moto(string p, string name, String nit):base(p,name,nit)
+        {
+        }
+        
+    }
+    public class Carro : Vehiculo
+    {
+        public Carro(string p, string name, String nit) : base(p, name, nit)
+        {
+        }
+
+    }
+    public class Camión : Vehiculo
+    {
+        public Camión(string p, string name, String nit) : base(p, name, nit)
+        {
+        }
+
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form2.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        static int m = 19;
+        static int c = 19;
+        static int ca = 19;
+        Moto[] mot = new Moto[m];
+        Carro[] car = new Carro[c];
+        Camión[] cam = new Camión[ca];
+        int ContadorM = 0;
+        int ContadorC = 0;
+        int Contadorca = 0;
+
+        public Form2()
+        {
+            InitializeComponent();
+          
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            checkBox1.Checked = true;
+            checkBox2.Checked = false;
+            checkBox3.Checked = false;
+        }
+
+      
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Equals(""))
+            {
+                MessageBox.Show("Faltan ingresar un Nombre");
+            }
+            else if (textBox2.Text.Equals(""))
+            {
+                MessageBox.Show("Faltan ingresar la Placa");
+            }
+            else if (textBox3.Text.Equals(""))
+            {
+                MessageBox.Show("Faltan ingresar el Nit");
+            }
+            else 
+            {
+                if (checkBox1.Checked == true)//Opcion Moto
+                {
+                    mot[ContadorM] = new Moto(textBox2.Text,textBox1.Text,textBox3.Text);
+                    ContadorM = ContadorM + 1;
+                }
+                else if (checkBox2.Checked == true)//Opcion Carro
+                {
+                    car[ContadorC] = new Carro(textBox2.Text, textBox1.Text, textBox2.Text);
+                    ContadorC = ContadorC + 1;
+                }
+                else if (checkBox3.Checked == true)//Opcion Camion
+                {
+                    cam[ContadorC] = new Camión(textBox2.Text, textBox1.Text, textBox2.Text);
+                    Contadorca = Contadorca + 1;
+                }
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            checkBox1.Checked = false;
+            checkBox2.Checked = true;
+            checkBox3.Checked = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            checkBox3.Checked = true;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            for (int x = 0; x < ContadorM; x++)
+            {
+                
+                Console.WriteLine(mot[x].Placa);
+            }
+        }
+    }
+}
